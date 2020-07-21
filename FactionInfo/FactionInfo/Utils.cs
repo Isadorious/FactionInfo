d665fc6f4d0384410c0cc64ec17367a4c24ab2e4
Isadorious <47084294+Isadorious@users.noreply.github.com>
2020-07-21 01:19:59 +0100

    Create utils class

---
+++ FactionInfo/FactionInfo/Utils.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VRage.Game.ModAPI;
+using Sandbox.Game.World;
+
+namespace FactionInfo
+{
+    public static class Utils
+    {
+
+        // From Essentials
+        public static IMyPlayer GetPlayerByNameOrId(string nameOrPlayerId)
+        {
+            if (!long.TryParse(nameOrPlayerId, out long id))
+            {
+                foreach (var identity in MySession.Static.Players.GetAllIdentities())
+                {
+                    if (identity.DisplayName == nameOrPlayerId)
+                    {
+                        id = identity.IdentityId;
+                    }
+                }
+            }
+
+            if (MySession.Static.Players.TryGetPlayerId(id, out MyPlayer.PlayerId playerId))
+            {
+                if (MySession.Static.Players.TryGetPlayerById(playerId, out MyPlayer player))
+                {
+                    return player;
+                }
+            }
+
+            return null;
+        }
+    }
+}
